527b1aa0b668de123b15d192abc950a8ae43d39c
michaelchang <msaechang1@gmail.com>
2020-06-28 22:25:05 -0700

    made style, script and index files

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+body {
+	background-color: #3B3B98;
+	color: #fff;
+	display: flex;
+	font-family: 'Muli', sans-serif;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 10px;
+	min-height: 100vh;
+}
