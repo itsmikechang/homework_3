77b0596c02127f8c5b54ffa4b5193509f854e945
michaelchang <msaechang1@gmail.com>
2020-06-30 08:09:49 -0700

    working on generate function

--- assets/script.js
+++ assets/script.js
@@ -27,9 +27,9 @@ function grabRandomUpper() {
 //console.log(grabRandomUpper());
 
 function grabRandomNumber() {
-  return +String.fromCharCode(Math.floor(Math.random()
-  * 10) + 48);
-}
+    return +String.fromCharCode(Math.floor(Math.random()
+    * 10) + 48);
+  }
 
 //console.log(grabRandomNumber());
 
@@ -39,4 +39,14 @@ function grabRandomSymbol() {
     symbols.length)];
 }
 
-console.log(grabRandomSymbol ());
+//console.log(grabRandomSymbol ());
+
+resultsEl.innerText = generatePassword(lower, upper, number, symbol) 
+    let generatedPassword = '';
+    var typesCount = lower + upper + number + symbol;
+
+    console.log('typesCount: ', typesCount);
+    var typesArr = [{lower}, {upper}, {number},
+    {symbol}].filter(function(item) {return Object.values(item)[0]});
+
+console.log('typesArr: ', typesArr);
