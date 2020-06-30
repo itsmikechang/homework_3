304ac5c3f81071f005690c099b82571845c1bbe8
michaelchang <msaechang1@gmail.com>
2020-06-30 08:41:45 -0700

    some type of addevent listener error?

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,40 @@
-var upperEl = document.getElementById('upper');
-var lowerEl = document.getElementById('lower');
-var numbersEl = document.getElementById('numbers');
-var symbolsEl = document.getElementById('symbols');
-var resultsEl = document.getElementById('results');
-var generateEl = document.getElementById('generate');
+const upperEl = document.getElementById('upper');
+const lowerEl = document.getElementById('lower');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const resultEl = document.getElementById('result');
+const generateEl = document.getElementById('generate');
+const lengthEl = document.getElementById('length');
 
-var randomFunc = {
+
+const randomFunc = {
     lower: grabRandomLower,
     upper: grabRandomUpper,
     number: grabRandomNumber,
     symbol: grabRandomSymbol
+}
+
+generateEl.addEventListener('click', () => {
+  const hasLower = lengthEl.checked;
+  const hasUpper = lengthEl.checked;
+  const hasNumber = lengthEl.checked;
+  const hasSymbol = lengthEl.checked;
+
+  resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+}
+  );
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  var typesCount = lower + upper + number + symbol;
+
+  // console.log('typesCount: ', typesCount);
+
+  var typesArr = [{lower}, {upper}, {number}, {symbol}].filter
+  
+  (function(item) {return Object.values(item)[0]});
+
+console.log('typesArr: ', typesArr);
 }
 
 function grabRandomLower() {
@@ -41,12 +66,3 @@ function grabRandomSymbol() {
 
 //console.log(grabRandomSymbol ());
 
-resultsEl.innerText = generatePassword(lower, upper, number, symbol) 
-    let generatedPassword = '';
-    var typesCount = lower + upper + number + symbol;
-
-    console.log('typesCount: ', typesCount);
-    var typesArr = [{lower}, {upper}, {number},
-    {symbol}].filter(function(item) {return Object.values(item)[0]});
-
-console.log('typesArr: ', typesArr);
